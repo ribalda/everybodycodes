54aada79dc31ef05c3ca6467e12abadfce9ab2ec
Ricardo Ribalda <ricardo@ribalda.com>
2025-11-26 19:59:20 +0100

    2025/17: Use levels

--- 2025/17/step1_2_3.py
+++ 2025/17/step1_2_3.py
@@ -58,7 +58,7 @@ def biggest_ring(pos, world):
         last_blast = new_blast
 
 
-def print_distances(vis, world):
+def print_distances(vis, level, world):
     print("")
     for i in range(100):
         if complex(i, 0) not in world:
@@ -67,7 +67,7 @@ def print_distances(vis, world):
         for j in range(100):
             if complex(i, j) not in world:
                 continue
-            kk = vis.pop(complex(i, j), -1)
+            kk = vis.pop((complex(i, j), level), -1)
             out += "{0:4d}".format(kk)
         print(out)
 
@@ -78,24 +78,25 @@ def shortest_circle(v, s, world, area):
     burnt = blast_area(area, v)
     max_dist = 30 * (area + 1)
 
-    visited_dist = [dict(), dict()]
-    has_crossed = 0
+    visited_dist = dict()
     dist = 0
     dummy = 0
 
-    heappush(todo, (dist, dummy, has_crossed, s))
+    heappush(todo, (dist, dummy, (s, 0)))
     while todo:
-        dist, _, has_crossed, pos = heappop(todo)
-        if has_crossed and pos == s:
-            # print_distances(visited_dist[0], world)
-            # print_distances(visited_dist[1], world)
+        dist, _, pos_level = heappop(todo)
+        if pos_level == (s, 1):
+            # print_distances(visited_dist,0, world)
+            # print_distances(visited_dist,1, world)
             return dist
 
-        if pos in visited_dist[has_crossed] and visited_dist[has_crossed][pos] <= dist:
+        if visited_dist.get(pos_level, max_dist) <= dist:
             continue
 
-        visited_dist[has_crossed][pos] = dist
+        visited_dist[pos_level] = dist
         for d in complex(1, 0), complex(-1, 0), complex(0, 1), complex(0, -1):
+            pos, level = pos_level
+
             new_pos = pos + d
             if new_pos not in world or new_pos in burnt:
                 continue
@@ -104,28 +105,26 @@ def shortest_circle(v, s, world, area):
                 continue
 
             if new_pos.imag == v.imag and new_pos.real > v.real:
-                if pos.imag > v.imag and not has_crossed:
+                if pos.imag > v.imag and level == 0:
                     continue
-
+            new_level = level
             if pos.imag == v.imag and pos.real > v.real:
-                if new_pos.imag < v.imag and has_crossed:
-                    continue
-                if new_pos.imag > v.imag and not has_crossed:
-                    dummy += 1
-                    heappush(todo, (new_dist, dummy, 1, new_pos))
-
+                if new_pos.imag > v.imag:
+                    new_level = 1
+                elif new_pos.imag < v.imag:
+                    new_level = 0
             dummy += 1
-            heappush(todo, (new_dist, dummy, has_crossed, new_pos))
+            heappush(todo, (new_dist, dummy, (new_pos, new_level)))
 
     return None
 
 
 def step3(v, s, world):
-    i = 0
-    while True:
+
+    for i in range(int(v.real)):
         out = shortest_circle(v, s, world, i)
+        # print(i, out)
         if out != None:
-            # print(i, out)
             return i * out
         i += 1
 
